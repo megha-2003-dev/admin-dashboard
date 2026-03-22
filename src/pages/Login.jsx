377691bda8a99af760f1995a98a428fc95daef6f
megha-2003-dev <meghas7401347@gmail.com>
2026-03-22 13:48:28 +0530

    Navigation button added for dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function Login(){
-    return <h2>Login Page</h2>
+    const navigate = useNavigate();
+
+    return(
+        <div>
+            <h1>Login Page</h1>
+
+            <button onClick={() => navigate("/dashboard")} className="bg-blue-500 text-white px-4 py-2 mt-4">
+                Go to Dashboard
+            </button>
+        </div>
+    )
 }
 
 export default Login;
